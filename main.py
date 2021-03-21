42db6173838d4f14dd5aaecb6f55909654b905ee
Nocker29 <49701710+Nocker29@users.noreply.github.com>
2021-03-21 15:50:13 +0100

    zad5 z błędami

--- main.py
+++ main.py
@@ -31,3 +31,31 @@
 # print(zakupy.ile_produktu())
 # print(zakupy.ile_kosztuje())
 
+# #zad5
+# class ciagi_arytmetyczne:
+#     def __init__(self,ciag):
+#         self.ciag=ciag
+#         self.a1=''
+#         self.r=''
+#     def wyswietl_dane(self):
+#         return self.ciag
+#     def pobierz_parametry(self,a1,r,ilosc):
+#         self.a1=a1
+#         self.r=r
+#         self.ilosc=ilosc
+#     def policz_sume(self):
+#         suma=0
+#         for x in range(0,len(self.ciag),1):
+#             suma+=self.ciag[x]
+#         return suma
+#     def policz_elementy(self):
+#         if (self.a1!='' & self.r!=''):
+#             return len(self.ciag)
+#         else:
+#             return 'Nie ma podanych a1 i r'
+# ciag=[1,3,5,7,9]
+# ciagi=ciagi_arytmetyczne(ciag)
+# print(ciagi.policz_sume())
+# print(ciagi.policz_elementy())
+# ciagi.pobierz_parametry(1,2,5)
+# print(ciagi.policz_elementy())
